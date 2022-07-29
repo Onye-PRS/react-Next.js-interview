b60be6f7042415e15f2df90a8741ba03d2e140c6
Luis Martinez <mrluismartinezzz@gmail.com>
2022-07-29 03:09:45 -0700

    styling reminder tag

--- src/calendar/Reminder.jsx
+++ src/calendar/Reminder.jsx
@@ -3,8 +3,8 @@ import { setSelectedDate } from "../redux/selectedDateSlice";
 import { deleteReminder } from "../redux/remindersSlice";
 import { Tag } from "antd";
 import { colorMap } from "../util/colorMap";
-import styles from "./reminder.module.scss"
-import dayjs from 'dayjs'
+import styles from "./reminder.module.scss";
+import dayjs from "dayjs";
 
 export const Reminder = ({ reminder, setVisible }) => {
   const dispatch = useDispatch();
@@ -16,7 +16,11 @@ export const Reminder = ({ reminder, setVisible }) => {
   };
 
   return (
-    <div key={reminder.date} onClick={handleOnClick} className={styles.reminder}>
+    <div
+      key={reminder.date}
+      onClick={handleOnClick}
+      className={styles.reminder}
+    >
       <Tag
         closable
         onClose={(e) => {
@@ -24,10 +28,12 @@ export const Reminder = ({ reminder, setVisible }) => {
           e.preventDefault();
           dispatch(deleteReminder(reminder));
         }}
-        style={{ width: "100%" , overflowX: "hidden"}}
+        style={{ width: "100%", overflowX: "hidden" }}
         color={colorMap[reminder.color]}
       >
-        {dayjs(reminder.date).format("hh:mm A")}{" "} <strong>{reminder.message}</strong>
+        {dayjs(reminder.date).format("h:mma")}
+        <br />
+        <strong> {reminder.message}</strong>
       </Tag>
     </div>
   );
